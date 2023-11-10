729543359433e2ceb1a4111b70142ce2f8cbc5e7
pjy <the001125@gmail.com>
2023-11-10 15:39:25 +0900

    component

---
+++ src/App_jsx.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+
+
+function App() {
+  let [listData,setListData] = useState(1)
+  return (
+    <div className="App">
+      <div>{listData}</div>
+      
+      <button onClick={()=>{setListData(listData+1)}}>click</button>
+
+    </div>
+  );
+}
+
+export default App;
