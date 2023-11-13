2c7f0a02e144e1cdf762425439e7630b0cce1234
pjy <the001125@gmail.com>
2023-11-13 17:18:30 +0900

    aa

---
+++ src/App props.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import './App.css'
+
+import ModalView from "./components/ModalView.jsx"
+function App() {
+  const [num, setNum] = useState(0)
+  const [data, setData] = useState([
+    "1",
+    "",
+    "",
+  ])
+  // function handlerTab() {
+  //   setNum(1)
+  // }
+  return (
+    <div className="App">
+      {/* <ModalView/> */}
+      {num}
+
+      <div className='tab'>
+        <ul>
+          <li onClick={() => { setNum(0) }} className={`${num == 0 ? "active" : ""}`}>tab1</li>
+          <li onClick={() => { setNum(1) }} className={`${num == 1 ? "active" : ""}`}>tab2</li>
+          <li onClick={() => { setNum(2) }} className={`${num == 2 ? "active" : ""}`}>tab3</li>
+        </ul>
+      </div>
+      <div style={{ margin: "0 0 10px 0" }}>
+        {data[num]}
+      </div>
+      <hr />
+      <ul>
+        {
+          data.map(function (item) {
+            return (
+              <>
+               <li>{item}</li>
+              </>
+            )
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
+import { useState } from 'react';
 import './App.css'
 
-import ModalView from "./components/ModalView.jsx"
 function App() {
+  const [num, setNum] = useState(0)
+  const [data, setData] = useState([
+    "1. Lorem ipsum dolor sit amet.",
+    "2. Asperiores sequi laudantium non praesentium?",
+    "3. Ipsum iure pariatur laborum explicabo.",
+  ])
+  const [VModal, setVModal] = useState(true)
+
+  const openList = (i) => {
+    setNum(i)
+    setVModal(true)
+  }
+
+  const handleClose = () => {
+    setVModal(false)
+  }
+
   return (
-    <div className="App">
-      <ModalView/>
+    <div className='App'>
+      {num}
+      <ul className="lists">
+        {
+          data.map(function (item, i) {
+            return (
+              <>
+                <li onClick={() => { openList(i) }}>{item}</li>
+              </>
+            )
+          })
+        }
+      </ul>
+      <button onClick={() => { setVModal(!VModal) }}>보기/안보기</button>
+
+      {
+        VModal == true ? <Modal rData={data} rNum={num} onClose={handleClose} /> : null
+      }
+      
     </div>
-  );
-}   
+  )
+}
+
+// function Modal(props) {
+//   return (
+//     <div className='modal'>
+//       <h3>안녕하세요</h3>
+//       <p>{props.rData[props.rNum]}</p>
+//       <div className='btnWrap'>
+//         <button>닫기</button>
+//       </div>
+//     </div>
+//   )
+// }
+function Modal({ rData, rNum, onClose }) {
+  return (
+    <div className='modal'>
+      <h3>안녕하세요</h3>
+      <p>{rData[rNum]}</p>
+      <div className='btnWrap'>
+        <button onClick={onClose}>닫기</button>
+      </div>
+    </div>
+  )
+}
+
 
 export default App;
